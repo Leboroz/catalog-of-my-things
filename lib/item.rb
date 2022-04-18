ce1213b9e5ffdc19e93138c8ea960bfcd676f5c3
Nigonbol123@gmail.com <nigonbol123@gmail.com>
2022-04-18 16:28:44 -0400

    Add can be archive method into item class

--- lib/item.rb
+++ lib/item.rb
@@ -9,4 +9,17 @@ class Item
     @publish_date = publish_date
     @archived = archived
   end
+
+	def can_be_archived?
+		current_year = Date.today().year
+		date = Date._parse(@publish_date)
+		publish_year = current_year - date[:year]
+		puts publish_year
+		publish_year > 10
+	end
+	
 end
+
+item = Item.new("1980-08-01", true)
+
+puts item.can_be_archived?
