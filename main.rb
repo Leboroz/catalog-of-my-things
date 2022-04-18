f23a83b58c5e9519650e40a04b07da81998eaa98
lisandroseia <lisandroseia11@gmail.com>
2022-04-18 17:57:57 -0300

    Implement main class and start the app

---
+++ main.rb
@@ -0,0 +1,24 @@
+class Main
+  def start
+    puts 'Welcome to School Library!'
+    loop do
+      puts "Please choose an option by entering a number
+1-List all books
+2-List all music albums
+3-List of games
+4-List all genres (e.g 'Comedy', 'Thriller')
+5-List all labels (e.g. 'Gift', 'New')
+6-List all authors (e.g. 'Stephen King')
+7-Add a book
+8-Add a music album
+9-Add a game
+10-Exit"
+      num = gets.chomp.to_i
+      break if num == 10
+    end
+  end
+end
+
+game = Main.new
+
+game.start
