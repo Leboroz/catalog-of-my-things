04f1bbb0a7ce999818e38cd29591f69798c4192a
Nigonbol123@gmail.com <nigonbol123@gmail.com>
2022-04-19 16:09:03 -0400

    Modify arguments value || Created boolean checker method

--- lib/music/music_album.rb
+++ lib/music/music_album.rb
@@ -1,14 +1,14 @@
 require_relative '../item'
 
 class MusicAlbum < Item
-	attr_accessor :name, :publish_date, :archive, :on_spotify
-	
-	def initialize(name, publish_date, archive, on_spotify)
-		super(name, publish_date, archive)
-		@on_spotify = on_spotify
-	end
+  attr_accessor :name, :publish_date, :archive, :on_spotify
 
-	def can_be_archived?
-		super && @on_spotify == true
-	end
+  def initialize(name, publish_date, archived, on_spotify)
+    super(name, publish_date, archived)
+    @on_spotify = on_spotify
+  end
+
+  def can_be_archived?
+    super && @on_spotify == true
+  end
 end
--- lib/music/music_manager.rb
+++ lib/music/music_manager.rb
@@ -2,69 +2,61 @@ require_relative 'genre'
 require_relative 'music_album'
 
 class MusicManager
-	attr_accessor :list_gender, :list_album
-	
-	def initialize
-		@list_gender = []
-		@list_album = []
-	end
-	
-	def genre_menu
-		puts "Add a new Music Album\n\n"
+  attr_accessor :list_gender, :list_album
 
-		puts "Album genre name:"
-		genre_name = gets.chomp
-		genre = Genre.new(genre_name)
-		@list_gender << genre
-		genre
-	end
+  def initialize
+    @list_gender = []
+    @list_album = []
+  end
 
-	def album_menu
-		puts "Album name: "
-		album_name = gets.chomp.capitalize
-		
-		puts "Date of publication example formta: [YYYY-MM-DD]"
-		date = gets.chomp
-		
-		puts "Can be archived? [Y/N]"
-		archive = gets.chomp.upcase
-		
-		case archive
-		when 'Y'
-			archive = true
-		when 'N'
-			archive = false
-		else
-			puts "Invalid option"
-		end
-		
-		puts "Its in spotify? [Y/N]"
-		spotify = gets.chomp.upcase
-		case spotify
-			when 'Y'
-				spotify = true
-			when 'N'
-				spotify = false
-			else
-				puts "Invalid option"
-		end 
-	
-		album = MusicAlbum.new(album_name, date, archive, spotify)
-		puts "Album created succesfully"
-		@list_album << album
-	end
+  def validated_boolean(option)
+    case option
+    when 'Y'
+      true
+    when 'N'
+      false
+    else
+      puts 'Invalid option'
+    end
+  end
 
-	def list_genre
-		@list_gender.each do |genre|
-			puts "ID: #{genre.id}-) genre: #{genre.name}"
-		end
-	end
+  def genre_menu
+    puts "Add a new Music Album\n\n"
 
-	def list_albums
-		@list_album.each do |album|
-			puts "ID: #{album.id}-) album name: #{album.name} publication date: #{album.publish_date}"
-		end
-	end
+    puts 'Album genre name:'
+    genre_name = gets.chomp
+    genre = Genre.new(genre_name)
+    @list_gender << genre
+    genre
+  end
+
+  def album_menu
+    puts 'Album name: '
+    album_name = gets.chomp.capitalize
+    puts 'Date of publication example formta: [YYYY-MM-DD]'
+    date = gets.chomp
+    puts 'Can be archived? [Y/N]'
+    archived = gets.chomp.upcase
+    check_ar = validated_boolean(archived)
+    puts 'Its in spotify? [Y/N]'
+    spotify = gets.chomp.upcase
+    check_spoti = validated_boolean(spotify)
+    album = MusicAlbum.new(album_name, date, check_ar, check_spoti)
+    puts 'Album created succesfully'
+    @list_album << album
+  end
+
+  def list_genre
+    @list_gender.each do |genre|
+      puts "ID: #{genre.id}-) genre: #{genre.name}"
+    end
+  end
+
+  def list_albums
+    @list_album.each do |album|
+      puts "ID: #{album.id}-) album name: #{album.name} publication date: #{album.publish_date} "
+    end
+  end
 end
 
 music = MusicManager.new
@@ -74,5 +66,3 @@ music.album_menu
 
 music.list_genre
 music.list_albums
-
-
