1b147caf9f80277e08fb40400f7a3d58e7a3d0a5
Leonardo Albornoz <albornozleo95@gmail.com>
2022-04-19 16:15:44 -0400

    Add name to classes

--- lib/books/book.rb
+++ lib/books/book.rb
@@ -3,13 +3,23 @@ require_relative '../item'
 class Book < Item
   attr_accessor :publisher, :cover_state
 
-  def initialize(publish_date, archive, publisher, _cover_state)
-    super(publish_date, archive)
+  def initialize(name, publish_date, archive, publisher, cover_state)
+    super(name, publish_date, archive)
     @publisher = publisher
-    @cover_letter = cover_letter
+    @cover_state = cover_state
   end
 
   def can_be_archived?
-    super || cover_state == 'bad'
+    super || @cover_state == 'bad'
+  end
+
+  def to_s
+    "[Book]
+name: #{@name}
+publish date: #{@publish_date}
+archive: #{@archive}
+publisher: #{@publisher}
+cover state: #{@cover_state}
+    "
   end
 end
--- lib/books/label.rb
+++ lib/books/label.rb
@@ -1,15 +1,23 @@
 class Label
   attr_accessor :id, :title, :color, :items
 
-  def initialize(title, color, items, id = rand(1..1000))
+  def initialize(title, color, id = rand(1..1000))
     @id = id
     @title = title
     @color = color
-    @items = items
+    @items = []
   end
 
   def add_item(item)
     @items << item
     item.label = self
+  end
+
+  def to_s
+    "[Label]
+   Id: #{@id}
+Title: #{@title}
+Color: #{@color}
+     "
   end
 end
