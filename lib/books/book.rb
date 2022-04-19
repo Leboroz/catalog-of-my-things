796af2c0be275b2d3a71e77275a660e1d41e7f9b
Leonardo Albornoz <albornozleo95@gmail.com>
2022-04-19 12:03:23 -0400

    Fix linters

--- lib/books/book.rb
+++ lib/books/book.rb
@@ -3,7 +3,7 @@ require_relative '../item'
 class Book < Item
   attr_accessor :publisher, :cover_state
 
-  def initialize(publish_date, archive, publisher, cover_state)
+  def initialize(publish_date, archive, publisher, _cover_state)
     super(publish_date, archive)
     @publisher = publisher
     @cover_letter = cover_letter
