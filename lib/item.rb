042f8675de540310f3063b0695d6f35b66db86de
Leonardo Albornoz <albornozleo95@gmail.com>
2022-04-19 12:02:02 -0400

    Make changes

--- lib/item.rb
+++ lib/item.rb
@@ -13,7 +13,6 @@ class Item
     current_year = Date.today.year
     date = Date._parse(@publish_date)
     publish_year = current_year - date[:year]
-    puts publish_year
     publish_year > 10
   end
 
@@ -21,7 +20,3 @@ class Item
     @archived = true if can_be_archived?
   end
 end
-
-item = Item.new('1980-08-01', true)
-
-puts item.can_be_archived?
