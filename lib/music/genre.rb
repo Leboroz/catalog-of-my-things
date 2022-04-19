8bf6d9ca7491596ad70b2121a9a8fdf0ce3321b7
Nigonbol123@gmail.com <nigonbol123@gmail.com>
2022-04-19 11:56:31 -0400

    Create class Genre || define add item method

---
+++ lib/music/genre.rb
@@ -0,0 +1,15 @@
+class Genre 
+	attr_accessor :id, :name, :items
+
+	def initialize(name, items, id = rand(1..100))
+		@id = id
+		@name = name
+		@items = items
+	end
+	
+	def add_item(item)
+		@items.push(item)
+		item.genre= self
+	end
+
+end
