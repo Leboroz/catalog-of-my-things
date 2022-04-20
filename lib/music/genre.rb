54ca36f417dfd75603ecc9854c41c5eb0465c0df
Nigonbol123@gmail.com <nigonbol123@gmail.com>
2022-04-19 23:32:34 -0400

    Creadte to string and to json methods

--- lib/music/genre.rb
+++ lib/music/genre.rb
@@ -1,7 +1,7 @@
 class Genre
   attr_accessor :id, :name, :items
 
-  def initialize(name, id = rand(1..100))
+  def initialize(name, id = rand(1..1000))
     @id = id
     @name = name
     @items = []
@@ -11,4 +11,19 @@ class Genre
     @items.push(item)
     item.genre = self
   end
+
+	def to_s
+		"[genre]
+		ID: #{@id}
+		name: #{@name}
+		"
+	end
+
+	def to_json(*_args)
+		{
+			'id' => @id,
+			'name' => @name,
+			'items' => @items.map(&:id)
+		}
+	end
 end
