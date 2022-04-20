04bc1fd511ced9764c7775301de4f12c273f2e31
Nigonbol123@gmail.com <nigonbol123@gmail.com>
2022-04-19 23:33:34 -0400

    Include id argument into main constructor

--- lib/music/music_album.rb
+++ lib/music/music_album.rb
@@ -1,14 +1,36 @@
 require_relative '../item'
 
 class MusicAlbum < Item
-  attr_accessor :name, :publish_date, :archive, :on_spotify
+  attr_accessor :name, :publish_date, :archived, :on_spotify
 
-  def initialize(name, publish_date, archived, on_spotify)
-    super(name, publish_date, archived)
+  def initialize(name, publish_date, archived, on_spotify, id = rand(1..1000))
+    super(publish_date, archived, id)
     @on_spotify = on_spotify
+		@name = name
   end
 
   def can_be_archived?
     super && @on_spotify == true
   end
+
+	def to_s
+		"[album]
+		ID: #{@id}
+		name: #{@name}
+		archive: #{@archived}
+		published: #{@publish_date}
+		spotify: #{@on_spotify}
+		"
+	end
+
+	def to_json(*_args)
+		{
+			'id' => @id,
+			'name' => @name,
+			'published' => @publish_date,
+			'archived' => @archived,
+			'spotify' => @on_spotify
+		}
+	end
+
 end
