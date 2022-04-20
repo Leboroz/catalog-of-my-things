769d561608be2b2fec15af5e2d15729ec0f6575c
Nigonbol123@gmail.com <nigonbol123@gmail.com>
2022-04-19 23:35:26 -0400

    Add write file methods || handles read file implementation

--- lib/music/music_manager.rb
+++ lib/music/music_manager.rb
@@ -1,11 +1,14 @@
+require 'json'
+require 'fileutils'
+
 require_relative 'genre'
 require_relative 'music_album'
 
 class MusicManager
-  attr_accessor :list_gender, :list_album
+  attr_accessor :list_genre, :list_album
 
   def initialize
-    @list_gender = []
+    @list_genre = []
     @list_album = []
   end
 
@@ -20,14 +23,13 @@ class MusicManager
     end
   end
 
-  def genre_menu
+	def genre_menu
     puts "Add a new Music Album\n\n"
-
     puts 'Album genre name:'
     genre_name = gets.chomp
     genre = Genre.new(genre_name)
-    @list_gender << genre
-    genre
+    @list_genre << genre
+		genre
   end
 
   def album_menu
@@ -42,12 +44,14 @@ class MusicManager
     spotify = gets.chomp.upcase
     check_spoti = validated_boolean(spotify)
     album = MusicAlbum.new(album_name, date, check_ar, check_spoti)
+		genre = genre_menu
+		genre.add_item(album)
     puts 'Album created succesfully'
     @list_album << album
   end
 
   def list_genre
-    @list_gender.each do |genre|
+    @list_genre.each do |genre|
       puts "ID: #{genre.id}-) genre: #{genre.name}"
     end
   end
@@ -57,4 +61,48 @@ class MusicManager
       puts "ID: #{album.id}-) album name: #{album.name} publication date: #{album.publish_date} "
     end
   end
+
+	def to_album(albums)
+		albums.map do |item|
+			MusicAlbum.new(
+				item['name'],
+				item['published'],
+				item['archived'],
+				item['spotify'],
+				item['id']
+			)
+		end
+	end
+
+	def to_genre(genre)
+		genre.map do |item|
+			new_genre = Genre.new(
+				item['name'],
+				item['id']
+			)
+			item['items']. each {|e| new_genre.add_item(e)}
+			new_genre
+		end
+	end
+
+	def write_json
+		json = {
+			'albums' => @list_album.map(&:to_json),
+			'genre' => @list_genre.map(&:to_json)
+		}
+		File.write('music.json', JSON.pretty_generate(json))
+	end
+
+	def load_json
+		Fileutiles.touch('music.json') unless File.exist?('music.json')
+			json = File.read('music.json')
+			json = JSON.parse(json)
+			@list_album = to_album(json['albums'])
+			@list_genre = to_genre(json['genre'])
+	end
 end
+
+music = MusicManager.new
+
+music.album_menu
+music.write_json
